027e362996e1aef4cb76fedfd468a1c5a1fbc325
Mouhamad Abdallah <mouhamad.abdallah.92@gmail.com>
2016-09-11 07:11:31 +1000

    [Calculator Console Application - Interpreter Pattern]
    Created a console application that uses the interpreter pattern to evaluate a mathematical expression.
    There is no error handling at the moment and it is a rudementary implementation but it is able to correctly evaluate:
    -Operation Order
    -Signed values
    
    The code needs some refactoring but I want to think over it a bit more to see if there is a way to make an operation tree that can be executed or if there is a more intuitive way of evaluating the expressions.
    Currently the string is filtered, then ran through an expressions interface that is used to evaluate itself, and then converted into a string, and ran again for evaluation of remaining operations. So I'd like to refactor some of it later after sitting down and writing a bit of psuedocode to see if there is a cleaner way of approaching the problem and making the interpreter pattern implemented better in this example.

---
+++ Calculator/Calculator/Number.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Number : IExpression
+    {
+        private int value;
+        
+        public Number(int value)
+        {
+            this.value = value;
+        }
+
+        public int evaluate()
+        {
+            return value;
+        }
+    }
+}
---
+++ Calculator/Calculator/Operator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public enum Operation
+    {
+        ADDITION,
+        SUBTRACTION,
+        MULTIPLICATION,
+        DIVISION
+    }
+
+    class Operator : IExpression
+    {
+        private IExpression left, right;
+        private Operation op;
+
+        public Operator(IExpression left, IExpression right, Operation op)
+        {
+            this.left = left;
+            this.right = right;
+            this.op = op;
+        }
+
+        public int evaluate()
+        {
+            switch(op)
+            {
+                case Operation.ADDITION:
+                    return left.evaluate() + right.evaluate();
+                case Operation.SUBTRACTION:
+                    return left.evaluate() - right.evaluate();
+                case Operation.MULTIPLICATION:
+                    return left.evaluate() * right.evaluate();
+                case Operation.DIVISION:
+                    return left.evaluate() / right.evaluate();
+                default:
+                    return -1; //error somehow?
+            }
+            
+        }
+    }
+}
