9112753d8379cb46415da218977089a2dbbfdb44
Mouhamad Abdallah <mouhamad.abdallah.92@gmail.com>
2016-10-16 13:45:25 +1100

    [References and Null - Experiment]
    Experimented with ways of being able to pass linked references which if 1 were set to null the rest would be too. However it seems that reference types in C# are unique when assigned to the instance object. So it is not possible to easily destroy or lose a reference to an instance of an object.

---
+++ FactoryReferences/FactoryReferences/FactoryItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryReferences
+{
+    class FactoryItem
+    {
+        public string name;
+        public int ID;
+        public bool alive;
+
+        public FactoryItem(string name, int ID)
+        {
+            this.name = name;
+            this.ID = ID;
+            alive = true;
+        }
+
+        public void Destroy()
+        {
+            alive = false;
+        }
+
+        public static bool operator ==(FactoryItem a, FactoryItem b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(FactoryItem a, FactoryItem b)
+        {
+            return !a.Equals(b);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return (alive & base.Equals(obj));
+        }
+    }
+}
---
+++ FactoryReferences/FactoryReferences/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryReferences
+{
+    class Program
+    {
+        private static List<FactoryItem> objects;
+
+        static void Main(string[] args)
+        {
+            FactoryItem player = null;
+            Create("Bob", ref player);
+            FactoryItem girl = null;
+            Create("Claire", ref girl);
+            FactoryItem referer = player;
+            DisplayPlayerInfo(player);
+            DisplayPlayerInfo(girl);
+            Destroy(ref player);
+            DisplayPlayerInfo(player);
+            DisplayPlayerInfo(referer);
+            Console.ReadLine();
+        }
+
+        static void DisplayPlayerInfo(FactoryItem item)
+        {
+            bool isValid = !item.Equals(null);
+            Console.WriteLine("Player Ref? " + isValid);
+            if (isValid)
+            {
+                Console.WriteLine("Player alive? " + item.alive);
+                Console.WriteLine("Player name: " + item.name);
+                Console.WriteLine("Player ID: " + item.ID);
+            }
+        }
+
+        static void Destroy(ref FactoryItem item)
+        {
+            if(objects != null)
+            {
+                int index = objects.IndexOf(item);
+                objects[index] = null;
+                objects.RemoveAt(index);
+                Console.WriteLine(objects.Count);
+                item.Destroy();               
+                //item = null;
+            }
+        }
+
+        static void Create(string name, ref FactoryItem pointer)
+        {
+            if(objects != null)
+            {
+                objects.Add(new FactoryItem(name, objects.Count));
+            }
+            else
+            {
+                objects = new List<FactoryItem>();
+                objects.Add(new FactoryItem(name, objects.Count));
+            }
+            pointer = objects[objects.Count - 1];
+        }
+    }
+}
